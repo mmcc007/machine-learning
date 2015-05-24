ee56e447582badcfa7d179afc6633218779e826c
Maurice McCabe <github@orbsoft.com>
2015-05-24 16:14:33 -0700

    Remove duplicated directory

--- machine-learning/mlclass-ex5-007/mlclass-ex5/trainingError.m
+++
@@ -1,9 +0,0 @@
-function [ Jt Jval ] = trainingError( X, y )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-%  Train linear regression with lambda = 0
-lambda = 0;
-[theta] = trainLinearReg([ones(m, 1) X], y, lambda);
-Jt = linearRegCostFunction(X, y, theta, lambda)
-end
-
